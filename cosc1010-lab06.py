0c18a6eb0185d961f8068dc0588d94827687ffd7
Existingperson26 <156600711+Existingperson26@users.noreply.github.com>
2024-10-17 15:56:12 -0600

    Update cosc1010-lab06.py

--- cosc1010-lab06.py
+++ cosc1010-lab06.py
@@ -1,9 +1,9 @@
-# Your Name Here
+# Peter Martinez
 # UWYO COSC 1010
-# Submission Date
+# 10/17/2024
 # Lab 06
-# Lab Section: 
-# Sources, people worked with, help given to: 
+# Lab Section: 12
+# Sources, people worked with, help given to: Chauncy Hendon, Cole Jordan
 # your
 # comments
 # here
@@ -70,7 +70,13 @@ print(len(random_string)) # Print out the size for reference
 # Output which letter occurred the most 
 # Output which letter occurred the least 
 # Output what the percentage of the string each character is, again in alphabetical
-
+headache = {}
+for letter in random_string:
+    if letter not in headache:
+        headache[letter] = 1
+    else:
+        headache[letter] += 1
+print(headache)
 #Tips and trick:
 # You can iterate through strings like you would a list
 # All characters are lowercase 
@@ -84,12 +90,36 @@ print(len(random_string)) # Print out the size for reference
 #Will need to first declare a dictionary 
 
 # Output: each letter and its corresponding occurrence in alphabetical order
+print("*"*75)
+sortedLetters = []
+for char in random_string:
+    sortedLetters.append(char)
+sortedLetters.sort()
 
+headache2 = {}
+for letter2 in sortedLetters:
+    if letter2 not in headache2:
+        headache2[letter2] = 1
+    else:
+        headache2[letter2] += 1
+print(headache2)
 print("*"*75)
 # Output which letter occurred the most 
+storedVal = 0
+most_occurred = 0
+for key, value in headache.items():
+    if storedVal < value:
+        storedVal = value
+        most_occurred = key
 
-most_occurred = ""
-least_occurred = ""
+maximumVal = storedVal
+storedMin = 0
+print(maximumVal)
+for key, value in headache.items():
+    if maximumVal > value:
+        maximumVal = value
+        least_occurred = key
+
 
 print(f"The letter that occurred the most is {most_occurred}")
 print("*"*75)
@@ -98,3 +128,6 @@ print(f"The letter that occurred the most is {least_occurred}")
 print("*"*75)
 
 # Output what the percentage of the string each character is, again in alphabetical
+
+for key, value in headache2.items():
+    print(f"Percentage Occurrence: {value / len(random_string) * 100})%")
